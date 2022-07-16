4def5b6f7d7204b52663ec6c9be737ffd064f3fc
ljbernardo <leejordanbernardo@gmail.com>
2022-07-16 16:02:47 +0800

    Edit code

--- public/borrowers/index.php
+++ public/borrowers/index.php
@@ -10,7 +10,21 @@ try {
     $statement = $conn->prepare("SELECT * FROM jai_db.borrowers WHERE firstname LIKE :search OR middlename LIKE :search OR lastname LIKE :search OR b_id LIKE :search ORDER BY b_id ASC");
     $statement->bindValue(':search', "%$search%");
   } else {
-    $statement = $conn->prepare("SELECT * FROM jai_db.borrowers ORDER BY b_id ASC");
+    $statement = $conn->prepare("SELECT b.b_id, b.picture, b.firstname, b.middlename, b.lastname, b.address, b.contactno,
+                                        b.birthday, b.businessname, b.occupation, b.comaker, b.comakerno, b.remarks, b.datecreated,
+                                        l.l_id, l.amount, l.payable, l.balance, l.mode, l.term, l.interestrate, l.amortization,
+                                        l.releasedate, l.duedate, l.status, l.c_id
+                                 FROM jai_db.borrowers as b
+                                 LEFT JOIN jai_db.loans as l
+                                 ON b.b_id = l.b_id
+                                 UNION
+                                 SELECT b.b_id, b.picture, b.firstname, b.middlename, b.lastname, b.address, b.contactno,
+                                        b.birthday, b.businessname, b.occupation, b.comaker, b.comakerno, b.remarks, b.datecreated,
+                                        l.l_id, l.amount, l.payable, l.balance, l.mode, l.term, l.interestrate, l.amortization,
+                                        l.releasedate, l.duedate, l.status, l.c_id
+                                 FROM jai_db.borrowers as b
+                                 RIGHT JOIN jai_db.loans as l
+                                 ON b.b_id = l.b_id");
   }
 
 
@@ -26,6 +40,7 @@ try {
 // echo "<pre>";
 // var_dump($borrowers);
 // echo '</pre>';
+// exit;
 
 ?>
 
@@ -74,25 +89,35 @@ try {
         </div>
       </div>
       <div class="col">
-        <div class="row">
-          <div class="col">
-            <p class="jai-table-amount primary-font"><span class="jai-table-label">Amount:</span> ₱100,000.00</p>
+        <?php if (ucwords(strtolower($borrower['amount'])) == "") {
+        ?>
+          No active loan
+        <?php
+        } else {
+        ?>
+          <div class="row">
+            <div class="col">
+              <p class="jai-table-amount primary-font"><span class="jai-table-label">Amount:</span> <?= $amountValue = ucwords(strtolower($borrower['amount'])) == "" ? "N/A" : ucwords(strtolower($borrower['amount'])); ?></p>
+            </div>
+            <div class="col">
+              <p class="jai-table-payable primary-font"> <span class="jai-table-label">Payable: </span> <?= "₱" . ucwords(strtolower($borrower['payable'])) ?></p>
+            </div>
           </div>
-          <div class="col">
-            <p class="jai-table-payable primary-font"> <span class="jai-table-label">Payable: </span> ₱50,000.00</p>
+          <div class="row">
+            <div class="col">
+              <p class="jai-table-payment-made sub-font"> <span class="jai-table-label">Balance: </span> <?= "₱" . ucwords(strtolower($borrower['balance'])) ?></p>
+              <p class="jai-table-mode sub-font"> <span class="jai-table-label">Mode: </span> Weekly, 6 months</p>
+              <p class="jai-table-amort sub-font"> <span class="jai-table-label">Amortization: </span> ₱140.00</p>
+            </div>
+            <div class="col">
+              <p class="jai-table-release sub-font"> <span class="jai-table-label">Release Date: </span> 01/01/22</p>
+              <p class="jai-table-due sub-font"> <span class="jai-table-label">Due Date: </span> 01/01/22</p>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <p class="jai-table-payment-made sub-font"> <span class="jai-table-label">Payments made: </span> ₱50,000.00</p>
-            <p class="jai-table-mode sub-font"> <span class="jai-table-label">Mode: </span> Weekly, 6 months</p>
-            <p class="jai-table-amort sub-font"> <span class="jai-table-label">Amortization: </span> ₱140.00</p>
-          </div>
-          <div class="col">
-            <p class="jai-table-release sub-font"> <span class="jai-table-label">Release Date: </span> 01/01/22</p>
-            <p class="jai-table-due sub-font"> <span class="jai-table-label">Due Date: </span> 01/01/22</p>
-          </div>
-        </div>
+        <?php
+
+        } ?>
+
       </div>
       <div class="col position-relative">
         <textarea class="jai-table-input" type="text"></textarea>
