ce5f333e3cd90508deb87958e935d55faeae9712
ljbernardo <leejordanbernardo@gmail.com>
2022-07-21 22:37:51 +0800

    minor changes

---
+++ public/payments/addpayment.php
@@ -0,0 +1,172 @@
+<?php
+
+try {
+  /** @var $conn \PDO */
+  require_once "../../dbconn.php";
+
+  // echo "DB connected successfully";
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$statement = $conn->prepare("SELECT b.b_id, b.firstname, b.middlename, b.lastname
+                                FROM jai_db.borrowers as b
+                                LEFT JOIN jai_db.loans as l
+                                ON b.b_id = l.b_id 
+                                WHERE (b.isdeleted = 0) AND (l.amount IS NOT NULL)
+                                ORDER BY b.b_id ASC");
+$statement->execute();
+$loans = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+date_default_timezone_set("Asia/Manila");
+
+$errors = [];
+
+$b_id = '';
+$amount = '';
+$payable = '';
+$balance = '';
+$mode = '';
+$term = '';
+$interestrate = '';
+$amortization = '';
+$releasedate = '';
+$duedate = '';
+$status = '';
+
+$loan = [
+  'b_id' => '',
+  'amount' => '',
+  'payable' => '',
+  'balance' => '',
+  'mode' => '',
+  'term' => '',
+  'interestrate' => '',
+  'amortization' => '',
+  'releasedate' => '',
+  'duedate' => '',
+  'status' => '',
+];
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+//   require_once "../../validate_loan.php";
+
+//   if (empty($errors)) {
+
+//     $statement = $conn->prepare("INSERT INTO jai_db.borrowers (picture, firstname, middlename, lastname, address, contactno, birthday, businessname,
+//                                                                     occupation, comaker, remarks, datecreated)
+//                                                            VALUES (:picture, :firstname, :middlename, :lastname, :address, :contactno, :birthday, :businessname,
+//                                                                     :occupation, :comaker, :remarks, :datecreated)");
+
+//     $statement->bindValue(':picture', $picturePath);
+//     $statement->bindValue(':firstname', $firstname);
+//     $statement->bindValue(':middlename', $middlename);
+//     $statement->bindValue(':lastname', $lastname);
+//     $statement->bindValue(':address', $address);
+//     $statement->bindValue(':contactno', $contactno);
+//     $statement->bindValue(':birthday', $birthday);
+//     $statement->bindValue(':businessname', $businessname);
+//     $statement->bindValue(':occupation', $occupation);
+//     $statement->bindValue(':comaker', $comaker);
+//     $statement->bindValue(':remarks', $remarks);
+//     $statement->bindValue(':datecreated', date('Y-m-d H:i:s'));
+
+//     $statement->execute();
+
+//     header('Location: index.php');
+//   }
+// }
+
+
+?>
+
+<?php include_once "../../views/partials/header.php"; ?>
+
+<div class="content-container">
+  <p>
+    <a href="index.php" class="btn btn-secondary">Go back</a>
+  </p>
+  <h1>Add new payment</h1>
+
+  <?php
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $rAmount = floatval($_POST['amount']);
+    $rMode = $_POST['mode'];
+    $rTerm = $_POST['term'];
+
+    $statement = $conn->prepare("SELECT *
+                                  FROM jai_db.rates as r
+                                  WHERE (r.amount = '$rAmount') AND (r.mode = '$rMode') AND (r.term = '$rTerm')                                
+                                ");
+    $statement->execute();
+    $rates = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+    // echo $_POST['borrower'] . "<br>";
+    // echo $_POST['amount'] . "<br>";
+    // echo $_POST['mode'] . "<br>";
+    // echo $_POST['term'] . "<br>";
+
+
+    // echo "<pre>";
+    // var_dump($rates);
+    // echo "</pre>";
+
+
+    foreach ($rates as $i => $rate) {
+
+      echo "TEST <br>";
+      echo $_POST['borrower'] . '<br>';
+      echo 'Amount: ' . number_format($rate['amount'], 2) . '<br>';
+      echo 'Payable: ' . number_format($rate['payable'], 2) . '<br>';
+      echo 'Amortization: ' . number_format($rate['amortization']) . '<br>';
+      echo 'Interest Rate: ' . number_format($rate['interestrate'], 2) . '<br>';
+      echo ucwords(strtolower($rate['mode'])) . ', ' . ucwords(strtolower($rate['term'])) . '<br>';
+    }
+
+    // $statement2 = $conn->prepare("INSERT INTO jai_db.loans (b_id, amount, payable, balance, mode, term,
+    //                                                         interestrate, amortization, releasedate, duedate, status)
+    //                                                 VALUES (:b_id, :amount, :payable, :balance, :mode, :term,
+    //                                                         :interestrate, :amortization, :releasedate, :duedate, :status)");
+    
+    // $statement2->bindValue(':b_id', $_POST['borrower']);
+    // $statement2->bindValue(':amount', $rate['amount']);
+    // $statement2->bindValue(':payable', $rate['payable']);
+    // $statement2->bindValue(':balance', $rate['payable']);
+    // $statement2->bindValue(':mode', $rate['mode']);
+    // $statement2->bindValue(':term', $rate['term']);
+    // $statement2->bindValue(':interestrate', $rate['interestrate']);
+    // $statement2->bindValue(':amortization', $rate['amortization']);
+    // $statement2->bindValue(':releasedate', 'TEST');
+    // $statement2->bindValue(':duedate', 'TEST');
+    // $statement2->bindValue(':status', 'Active');
+
+    // $statement2->execute();
+
+    // header('Location: ../borrowers/index.php');
+
+  }
+
+  ?>
+
+  <br>
+
+  <!-- <script>
+    var existingNames = ["lee", "jordan", "angelo", "ivan", "willie", "ann"];
+
+    $("#namesearch").autocomplete({
+      source: existingNames
+    }, {
+      
+    });
+  </script> -->
+
+
+  <?php include_once "../../views/payments/form.php" ?>
+
+  </body>
+
+  </html>
---
+++ views/payments/form.php
@@ -0,0 +1,147 @@
+<?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger" role="alert">
+        <?php foreach ($errors as $error) : ?>
+            <div><?php echo $error; ?></div>
+        <?php endforeach; ?>
+    </div>
+
+<?php endif; ?>
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <script>
+        $(document).ready(function() {
+            $("#namesearch").keyup(function() {
+                var name = $("#namesearch").val();
+                $.ajax({
+                    url: "suggestions.php",
+                    method: "POST",
+                    data: {
+                        suggestion: name
+                    },
+                    dataType: "html",
+                    beforeSend: function() {},
+                    success: function(data) {
+
+                        //response (data);
+                        $("#borrower").html(data);
+                        console.log(data)
+                    },
+                    error: function(response) {
+                        console.log(response);
+                    },
+                });
+            });
+        });
+
+        // $( "#namesearch" ).autocomplete({
+        //   source: function( request, response ) {
+        //     $.ajax( {
+        //       url: "suggestions.php",
+        //       dataType: "jsonp",
+        //       data: {
+        //         term: request.term
+        //       },
+        //       success: function( data ) {
+        //         response( data );
+        //       }
+        //     } );
+        //   },
+        //   minLength: 2,
+        //   select: function( event, ui ) {
+        //     log( "Selected: " + ui.item.value + " aka " + ui.item.id );
+        //   }
+        // } );
+    </script>
+
+    <input data-borrower-name="" type="text" name="name" id="namesearch" placeholder="Search for borrowers...">
+    <br>
+    <span></span>
+    <select id="borrower" name="borrower">
+        <?php
+        foreach ($loans as $i => $loan) {
+            echo '<option>' . $loan['b_id'] . '</option>';
+        }
+        ?>
+    </select>
+
+    <!--
+    <?php if ($borrower['picture']) { ?>
+        <img src="/<?= 'JAI/public/' . $borrower['picture']; ?>" class="update-image">
+    <?php } ?>
+    -->
+
+
+    <?php // <img src="/<?= 'JAI/public/pictures/Default/picture-placeholder.png'; " class="update-image"> 
+    ?>
+
+    <br><br>
+    <div class="mb-3">
+        <label>Loan Amount</label>
+        <input name="loanamount" placeholder="Loan Amount" type="text" class="form-control" readonly value="TEST">
+    </div>
+    <div class="mb-3">
+        <label>Remaining Balance</label>
+        <input name="releasedate" placeholder="Remaining Balance" type="text" class="form-control" readonly>
+    </div>
+    <div class="mb-3">
+        <label>Amount</label>
+        <select class="form-control" name="amount">
+            <option>5000</option>
+            <option>10000</option>
+            <option>12000</option>
+            <option>14000</option>
+            <option>15000</option>
+            <option>20000</option>
+            <option>25000</option>
+            <option>30000</option>
+            <option>35000</option>
+            <option>40000</option>
+            <option>45000</option>
+            <option>50000</option>
+            <option>55000</option>
+            <option>60000</option>
+            <option>65000</option>
+            <option>70000</option>
+            <option>75000</option>
+            <option>80000</option>
+            <option>85000</option>
+            <option>90000</option>
+            <option>95000</option>
+            <option>100000</option>
+        </select>
+        <!-- <input placeholder="Amount" type="text" class="form-control" name="amount" value="<?php echo $amount ?>"> -->
+    </div>
+    <div class="mb-3">
+        <label>Mode</label>
+        <select class="form-control" name="mode">
+            <option>Daily</option>
+            <option>Weekly</option>
+            <option>Monthly</option>
+        </select>
+        <!-- <input placeholder="Mode" type="text" class="form-control" name="mode" value="<?php echo $mode ?>"> -->
+    </div>
+    <div class="mb-3">
+        <label>Term</label>
+        <select class="form-control" name="term">
+            <option>1 month</option>
+            <option>2 months</option>
+            <option>3 months</option>
+            <option>4 months</option>
+            <option>5 months</option>
+            <option>6 months</option>
+        </select>
+        <!-- <input placeholder="Term" type="text" class="form-control" name="term" value="<?php echo $term ?>"> -->
+    </div>
+    <div class="mb-3">
+        <label>Release Date</label>
+        <input name="releasedate" placeholder="Release Date" type="text" class="form-control" readonly>
+    </div>
+    <div class="mb-3">
+        <label>Due Date</label>
+        <input name="duedate" placeholder="Due Date" type="text" class="form-control">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
