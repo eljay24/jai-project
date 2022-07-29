f8b9555c5996bcb3e93f1b9fed5314585eda57d6
ljbernardo <leejordanbernardo@gmail.com>
2022-07-30 07:13:36 +0800

    minor changes

--- public/loans/create.php
+++ public/loans/create.php
@@ -13,7 +13,7 @@ $statement = $conn->prepare("SELECT DISTINCT b.b_id, b.firstname, b.middlename, 
                                 FROM jai_db.borrowers as b
                                 LEFT JOIN jai_db.loans as l
                                 ON b.b_id = l.b_id 
-                                WHERE (b.isdeleted = 0 AND b.activeloan = 0)
+                                WHERE b.isdeleted = 0 AND b.activeloan = 0
                                 ORDER BY b.b_id ASC");
 $statement->execute();
 $loans = $statement->fetchAll(PDO::FETCH_ASSOC);
