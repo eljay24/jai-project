deab23f31b886725008d0578c9a468361b416201
ljbernardo <leejordanbernardo@gmail.com>
2022-08-13 22:13:39 +0800

    auto due date

--- public/ajax-calls/create-loan.php
+++ public/ajax-calls/create-loan.php
@@ -38,7 +38,7 @@ if (isset($_POST['b_id'])) {
   $term = $_POST['term'];
   $collector = $_POST['collector'];
   $release_date = $_POST['release-date'];
-
+  $due_date = date('Y-m-d', strtotime('+'.$term, strtotime($release_date)));
 
   // echo json_encode($b_id.$data_row.$borrower.$amount.$mode.$term.$collector.$release_date);
   // $comaker = $_POST['comaker'];
@@ -72,10 +72,10 @@ if (isset($_POST['b_id'])) {
     if (empty($errors)) {
 
       $statement2 = $conn->prepare("INSERT INTO jai_db.loans (b_id, amount, mode, term, payable, balance, interestrate, amortization, releasedate, duedate, status, c_id, activeloan)
-                                  VALUES (:b_id, :amount, :mode, :term, :payable, :balance, :interestrate, :amortization, :releasedate, :duedate, :status, :c_id, :activeloan);
-                                  UPDATE jai_db.borrowers
-                                  SET activeloan = 1
-                                  WHERE b_id = :b_id");
+                                    VALUES (:b_id, :amount, :mode, :term, :payable, :balance, :interestrate, :amortization, :releasedate, :duedate, :status, :c_id, :activeloan);
+                                    UPDATE jai_db.borrowers
+                                    SET activeloan = 1
+                                    WHERE b_id = :b_id");
 
       $statement2->bindValue(':b_id', $borrower);
       $statement2->bindValue(':c_id', $collector);
@@ -87,7 +87,7 @@ if (isset($_POST['b_id'])) {
       $statement2->bindValue(':mode',  $mode);
       $statement2->bindValue(':term', $term);
       $statement2->bindValue(':releasedate', $release_date);
-      $statement2->bindValue(':duedate', 'TEST');
+      $statement2->bindValue(':duedate', $due_date);
       $statement2->bindValue(':activeloan', 1);
       $statement2->bindValue(':status', 'Active');
 
