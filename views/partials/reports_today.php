1420a1fc36efaee17a269fed55e75907a6da3df4
Lee Bernardo <elaybernardo@gmail.com>
2023-11-30 01:36:51 +0800

    remove yesterday column

--- views/partials/reports_today.php
+++ views/partials/reports_today.php
@@ -16,23 +16,25 @@ try {
     JOIN jai_db.loans as l ON p.l_id = l.l_id
     WHERE l.activeloan = 1
     GROUP BY l_id, amount
-  ), Yesterday AS (
-    SELECT p.amount, p.b_id
-    FROM jai_db.payments as p
-    WHERE p.date = CURDATE() - INTERVAL 1 DAY
   )
+  -- , Yesterday AS (
+  --   SELECT p.amount, p.b_id, p.l_id
+  --   FROM jai_db.payments as p
+  --   WHERE p.date = CURDATE() - INTERVAL 1 DAY
+  -- )
   SELECT b.b_id, 
+        r.l_id, 
         b.firstname as firstname, 
         b.lastname as lastname, 
-        r.l_id, r.amount as mode, 
+        r.amount as mode, 
         r.mode as ptype, 
-        y.amount as payment_yesterday, 
+        -- y.amount as payment_yesterday, 
         r.amortization as amort, 
         p.amount as payment_today
   FROM ModeCTE as r
   JOIN jai_db.borrowers as b ON r.b_id = b.b_id
   JOIN jai_db.payments as p ON r.b_id = p.b_id
-  JOIN Yesterday as y ON r.b_id = y.b_id
+  -- JOIN Yesterday as y ON r.b_id = y.b_id
   WHERE (rnk = 1 AND r.amount != 0) AND (p.date = CURDATE()) AND (p.amount != 0)
   ORDER BY p.amount < r.amount DESC, p.amount DESC");
 
@@ -55,8 +57,8 @@ try {
       <div class="col">Borrower</div>
       <div class="col">Amortization</div>
       <div class="col">Mode</div>
-      <div class="col">Today</div>
-      <div class="col">Yesterday</div>
+      <div class="col">Payment</div>
+      <!-- <div class="col">Yesterday</div> -->
       <!-- <div class="col-1 text-center">Action</div> -->
     </div>
     <?php
@@ -88,11 +90,11 @@ try {
             <p class="primary-font <?= ((float)$mode_data['payment_today'] < (float)$mode_data['mode'] ? 'red-font' : 'green-font')  ?>">₱<?= $mode_data['payment_today'] ?></p>
           </div>
         </div>
-        <div class="col">
+        <!-- <div class="col">
           <div class="row">
-            <p class="primary-font <?= ((float)$mode_data['payment_yesterday'] < (float)$mode_data['mode'] ? 'red-font' : 'green-font')  ?>"><?= $mode_data['payment_yesterday'] == 0 ? 'PASS' : '₱' . $mode_data['payment_yesterday']  ?></p>
+            <p class="primary-font <?php //echo ((float)$mode_data['payment_yesterday'] < (float)$mode_data['mode'] ? 'red-font' : 'green-font')  ?>"><?php //echo $mode_data['payment_yesterday'] == 0 ? 'PASS' : '₱' . $mode_data['payment_yesterday']  ?></p>
           </div>
-        </div>
+        </div> -->
       </div>
     <?php $count++;
     } ?>
